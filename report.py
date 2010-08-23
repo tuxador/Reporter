49668304882d730bcd49d1566e5698e5c7637e9a
Raja Selvaraj <rajajs@gmail.com>
2010-08-23 11:47:08 +0530

    Report.py has a working text editor

--- report.py
+++ report.py
@@ -3,6 +3,7 @@
 """Use supplied dictionary of values and a template to fill in the template
 with mako and render the report to a pdf with rst2pdf"""
 
+import wx
 import subprocess
 import tempfile
 from mako.template import Template
@@ -37,8 +38,8 @@ class Report():
     def generate_pdf(self):
         """Creat pdf and return path to pdf file"""
         # get the temp names
-        tmp_rstfilename = tempfile.mkstemp(suffix='rst')[1]
-        tmp_pdffilename = tempfile.mkstemp(suffix='pdf')[1]
+        tmp_rstfilename = tempfile.mkstemp(suffix='.rst')[1]
+        tmp_pdffilename = tempfile.mkstemp(suffix='.pdf')[1]
 
         # write the raw_report as a file
         with open(tmp_rstfilename,'w') as fi:
@@ -56,12 +57,68 @@ class Report():
 
     def edit_report(self):
         """Present a simple editor to edit the raw_report"""
-        pass
+        app = wx.App()
+        ed = Editor(None, self.raw_report)
+        if ed.ShowModal() == wx.ID_OK:
+            self.raw_report = ed.text
+        ed.Destroy()
+        app.MainLoop()
 
-
-
-
+        print 'Raw report is'
+        print self.raw_report
     
+class Editor(wx.Dialog):
+    """A simple text editor"""
+    def __init__(self, parent, text):
+        wx.Dialog.__init__(self, parent, -1, 'Edit Report')
+
+        self.text = text
+        
+        panel = wx.Panel(self, -1)
+        panelsizer = wx.BoxSizer(wx.VERTICAL)
+        buttonsizer = wx.BoxSizer(wx.HORIZONTAL)
+        
+        self.textcontrol = wx.TextCtrl(panel, -1, style=wx.TE_MULTILINE|wx.TE_RICH)
+        self.cancel_button = wx.Button(panel, -1, 'Cancel')
+        self.done_button = wx.Button(panel, -1, 'Done')
+
+        self.cancel_button.Bind(wx.EVT_BUTTON, self.cancel)
+        self.done_button.Bind(wx.EVT_BUTTON, self.ondone)
+        
+        buttonsizer.Add(self.cancel_button, 0, wx.ALL, 10)
+        buttonsizer.Add(self.done_button, 0, wx.ALL, 10)
+        
+        panelsizer.Add(self.textcontrol, 10, wx.ALL|wx.EXPAND, 2)
+        panelsizer.Add(buttonsizer, 1, wx.ALL, 2)
+
+        panel.SetSizer(panelsizer)
+        self.Layout()
+
+        self.textcontrol.write(text)
+
+       # self.ShowModal()
+
+        
+    def cancel(self, event):
+        """Cancel and discard edits"""
+        self.EndModal(wx.ID_CANCEL)
+
+    def ondone(self, event):
+        """return the modified text"""
+        self.text = self.textcontrol.GetValue()
+        self.EndModal(wx.ID_OK)
+        
+
+def editor_test():
+    """Testing the editor"""
+    app = wx.App()
+    ed = Editor(None, 'This is a test')
+    if ed.ShowModal() == wx.ID_OK:
+        print ed.text
+    ed.Destroy()
+    
+    app.MainLoop()
+        
 
 def test():
     """Test the report module and demonstrate use"""
@@ -78,9 +135,15 @@ def test():
     rep = Report(template_file, vals)
     rep.generate_raw()
     print rep.raw_report
-    
+
+    rep.edit_report()
+    #print "path to pdf file"
+    #pdf =  rep.generate_pdf()
+    #print pdf
+    #subprocess.Popen(['evince', pdf])
 
 
 
 if __name__ == '__main__':
+    #editor_test()
     test()
